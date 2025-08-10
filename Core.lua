28de331d8f8a667afeba5b766bff20af068ddc57
NicolasYD <147659516+NicolasYD@users.noreply.github.com>
2025-08-10 10:54:04 +0200

    Added FrameSelector function.

--- Core.lua
+++ Core.lua
@@ -59,3 +59,72 @@ function DRT:TestModules()
         end
     end
 end
+
+
+function DRT:FrameSelector(onSelectCallback)
+	local frameChooserFrame = DRT.frameChooserFrame
+	local frameChooserBox = DRT.frameChooserBox
+	local focusName
+
+
+	local function StopFrameChooser()
+		if frameChooserFrame then
+			frameChooserFrame:SetScript("OnUpdate", nil)
+			frameChooserFrame:Hide()
+		end
+		if frameChooserBox then
+			frameChooserBox:Hide()
+		end
+	end
+
+
+	if not frameChooserFrame then
+		frameChooserFrame = CreateFrame("Frame", "DRT_FrameChooserFrame", UIParent)
+		DRT.frameChooserFrame = frameChooserFrame
+	end
+
+	if not frameChooserBox then
+		frameChooserBox = CreateFrame("Frame", "DRT_FrameChooserBox", frameChooserFrame, "BackdropTemplate")
+		frameChooserBox:SetFrameStrata("TOOLTIP")
+		frameChooserBox:SetBackdrop({
+			edgeFile = "Interface\\Tooltips\\UI-Tooltip-Border",
+			edgeSize = 12,
+			insets = { left = 0, right = 0, top = 0, bottom = 0 }
+		})
+		frameChooserBox:SetBackdropBorderColor(0, 1, 0)
+		frameChooserBox:Hide()
+		DRT.frameChooserBox = frameChooserBox
+	end
+
+	frameChooserFrame:Show()
+
+	frameChooserFrame:SetScript("OnUpdate", function()
+		if IsMouseButtonDown("RightButton") then
+			StopFrameChooser()
+		elseif IsMouseButtonDown("LeftButton") and focusName then
+			StopFrameChooser()
+			if onSelectCallback then
+				onSelectCallback(focusName)
+			end
+		else
+			local focus
+			local foci = GetMouseFoci()
+
+			if foci and #foci > 0 then
+				focus = foci[1]
+				focusName = focus:GetName()
+			else
+				focusName = nil
+			end
+
+			if focus and focusName then
+				frameChooserBox:ClearAllPoints()
+				frameChooserBox:SetPoint("BOTTOMLEFT", focus, "BOTTOMLEFT", -4, -4)
+				frameChooserBox:SetPoint("TOPRIGHT", focus, "TOPRIGHT", 4, 4)
+				frameChooserBox:Show()
+			else
+				frameChooserBox:Hide()
+			end
+		end
+	end)
+end
